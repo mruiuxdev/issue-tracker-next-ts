818efc15898a114eb2d554ed0278a9ce9e632163
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-10-31 06:58:55 +0200

    creating issue form with validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,71 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Controller, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
+	const {
+		register,
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IssueForm>({
+		resolver: zodResolver(createIssueSchema),
+	});
+	const [error, setError] = useState("");
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const router = useRouter();
+
+	const onSubmit = handleSubmit(async (data) => {
+		try {
+			setIsSubmitting(true);
+
+			await axios.post("/api/issues", data);
+
+			router.push("/issues");
+		} catch (error) {
+			setIsSubmitting(false);
+
+			setError("An unexecuted error occurred");
+		}
+	});
+
 	return (
-		<div className="max-w-xl space-y-3">
-			<TextField.Root>
-				<TextField.Input placeholder="Title" />
-			</TextField.Root>
-			<SimpleMDE placeholder="Description" />
-			<Button>Submit New Issue</Button>
+		<div className="max-w-xl">
+			{error && (
+				<Callout.Root color="red" className="mb-5">
+					<Callout.Text>{error}</Callout.Text>
+				</Callout.Root>
+			)}
+			<form className="space-y-3" onSubmit={onSubmit}>
+				<TextField.Root>
+					<TextField.Input placeholder="Title" {...register("title")} />
+				</TextField.Root>
+				<ErrorMessage>{errors.title?.message}</ErrorMessage>
+				<Controller
+					name="description"
+					control={control}
+					render={({ field }) => (
+						<SimpleMDE placeholder="Description" {...field} />
+					)}
+				/>
+				<ErrorMessage>{errors.description?.message}</ErrorMessage>
+				<Button type="submit" disabled={isSubmitting}>
+					Submit New Issue {isSubmitting && <Spinner />}
+				</Button>
+			</form>
 		</div>
 	);
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	sans: [
+		"Inter",
+		"ui-sans-serif",
+		"system-ui",
+		"-apple-system",
+		"system-ui",
+		"Segoe UI",
+		"Roboto",
+		"Helvetica Neue",
+		"Arial",
+		"Noto Sans",
+		"sans-serif",
+		"Apple Color Emoji",
+		"Segoe UI Emoji",
+		"Segoe UI Symbol",
+		"Noto Color Emoji",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
