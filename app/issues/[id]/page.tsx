7db90ae5978623a6f625e86534ee70c322478882
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-10-31 21:37:34 +0200

    Updating Issues

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,10 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Grid } from "@radix-ui/themes";
 import delay from "delay";
 import { notFound } from "next/navigation";
-import Markdown from "react-markdown";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
 	params: { id: string };
@@ -19,16 +20,14 @@ const IssueDetailsPage = async ({ params }: Props) => {
 	await delay(2000);
 
 	return (
-		<div>
-			<Heading>{issue.title}</Heading>
-			<Flex className="space-x-3" my="2">
-				<IssueStatusBadge status={issue.status} />
-				<Text>{issue.createdAt.toDateString()}</Text>
-			</Flex>
-			<Card className="prose mt-4">
-				<Markdown>{issue.description}</Markdown>
-			</Card>
-		</div>
+		<Grid columns={{ initial: "1", md: "2" }} gap="5">
+			<Box>
+				<IssueDetails issue={issue} />
+			</Box>
+			<Box>
+				<EditIssueButton issueId={issue.id} />
+			</Box>
+		</Grid>
 	);
 };
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,77 +1,13 @@
-"use client";
-
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
-import { createIssueSchema } from "@/app/validationSchema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Callout, TextField } from "@radix-ui/themes";
-import axios from "axios";
-import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { z } from "zod";
+import IssueFormSkeleton from "./loading";
 
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+const IssueForm = dynamic(() => import("../_components/IssueForm"), {
 	ssr: false,
+	loading: () => <IssueFormSkeleton />,
 });
 
-type IssueForm = z.infer<typeof createIssueSchema>;
-
 const NewIssuePage = () => {
-	const {
-		register,
-		control,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<IssueForm>({
-		resolver: zodResolver(createIssueSchema),
-	});
-	const [error, setError] = useState("");
-	const [isSubmitting, setIsSubmitting] = useState(false);
-	const router = useRouter();
-
-	const onSubmit = handleSubmit(async (data) => {
-		try {
-			setIsSubmitting(true);
-
-			await axios.post("/api/issues", data);
-
-			router.push("/issues");
-		} catch (error) {
-			setIsSubmitting(false);
-
-			setError("An unexecuted error occurred");
-		}
-	});
-
-	return (
-		<div className="max-w-xl">
-			{error && (
-				<Callout.Root color="red" className="mb-5">
-					<Callout.Text>{error}</Callout.Text>
-				</Callout.Root>
-			)}
-			<form className="space-y-3" onSubmit={onSubmit}>
-				<TextField.Root>
-					<TextField.Input placeholder="Title" {...register("title")} />
-				</TextField.Root>
-				<ErrorMessage>{errors.title?.message}</ErrorMessage>
-				<Controller
-					name="description"
-					control={control}
-					render={({ field }) => (
-						<SimpleMDE placeholder="Description" {...field} />
-					)}
-				/>
-				<ErrorMessage>{errors.description?.message}</ErrorMessage>
-				<Button type="submit" disabled={isSubmitting}>
-					Submit New Issue {isSubmitting && <Spinner />}
-				</Button>
-			</form>
-		</div>
-	);
+	return <IssueForm />;
 };
 
 export default NewIssuePage;
